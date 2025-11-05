74cbd7dda4ace2fcc86d874443336e44ec131c29
kc-clintone <kayseeclintone@gmail.com>
2025-11-06 00:52:19 +0300

    started implementing auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //implememting storage here soon
+  const loggedIn = !!localStorage.getItem('token')
+
+  //if a user logs out
+  function handleLogout() {
+    localStorage.removeItem('token');
+    window.location.href = '/login'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
